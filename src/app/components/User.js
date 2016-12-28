9a816ca4fc93226da693f12e4014dea9329e0854
nemanja <n.manot@yahoo.com>
2016-12-28 10:16:53 +0100

    Changess

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -60,7 +60,7 @@ import React from "react";
                          onClick={this.editUserName.bind(this)}
                          className="btn btn-default btn-xs"
                      >
-                         EDITt
+                         EDIT
                      </button>
                  </td>
                  <td>
