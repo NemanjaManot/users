f88dca3e7a130002d94c09f17ab881a12faaf7ae
nemanja <n.manot@yahoo.com>
2017-01-18 11:32:42 +0100

    html-webpack-plugin add

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,14 @@ var SRC_DIR = path.resolve(__dirname, 'src');
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var config = {
     entry: SRC_DIR + '/app/index.js',
     output: {
         path: DIST_DIR + '/app',
         filename: 'bundle.js',
-        publicPath: '/app/'
+        publicPath: '/'
     },
     module: {
         loaders: [
@@ -30,7 +32,8 @@ var config = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin('bundle.css', {allChunks: true})
+        new ExtractTextPlugin('bundle.css', {allChunks: true}),
+        new HtmlWebpackPlugin({template: './src/index.ejs'})
     ],
     devtool: 'inline-source-map'
 };
